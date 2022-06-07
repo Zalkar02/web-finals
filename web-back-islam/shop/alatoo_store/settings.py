0af3620b2b5da2306dc174c022759fcfc1c2bb29
Zuko <58260485+Zalkar02@users.noreply.github.com>
2022-06-07 18:52:14 +0600

    Update settings.py

--- web-back-islam/shop/alatoo_store/settings.py
+++ web-back-islam/shop/alatoo_store/settings.py
@@ -4,10 +4,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hqa^3p!t@96o53l$finb=#i96-qb!iqeqvxm%81*a$k&9jecru'
+# SECRET_KEY = 'hqa^3p!t@96o53l$finb=#i96-qb!iqeqvxm%81*a$k&9jecru'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['0.0.0.0', '192.168.1.103', '127.0.0.1']
 
